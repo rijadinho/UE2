4fb974f5ca0e38268f1148b6d176f1da73fa3fe1
Rijad Kovacevic <rijad.kovacevic@student.tugraz.at>
2017-11-26 21:51:16 +0100

    Added few new functions

--- ass2.c
+++ ass2.c
@@ -18,12 +18,17 @@
 
 typedef struct _Elements_
 {
-  char title_[255];
-  char text_;
+  char *title_;
+  char *text_;
   char *choice_A_;
   char *choice_B_;
 
 } Elements;
+
+char openFile(Elements element, char* input);
+char choiceA();
+char choiceB();
+char enterCommand();
 
 int main (int argc, char* argv[])
 {
@@ -35,16 +40,85 @@ int main (int argc, char* argv[])
     return 1;
   }
 
-  FILE* file_open = fopen (argv[1], "r");
+  openFile(element, argv[1]);
+  
+  if(EOF)
+    enterCommand();
+  else
+    return 0;
+}
+
+char openFile(Elements element, char* input)
+{
+  FILE* file_open = fopen(input, "r");
 
   if (file_open == NULL)
   {
-    printf("[ERR] Could not read file %s.\n", argv[1]);
+    printf("[ERR] Could not read file %s.\n", input);
     return 3;
   }
 
-  while((element.text_ = fgetc(file_open)) != EOF)
-    printf("%c", element.text_);
-    
+  // Print 30 x lines at the beginning of the file
+  for (int counter = 0; counter < 30; counter++)
+  {
+    printf("-");
+  }
+  // New line
+  printf("\n");
+
+  char line[256];
+  int line_counter = 0;
+
+  while(fgets(line, sizeof(line), file_open))
+  {
+    printf("%s %d", line, line_counter);
+
+    if (line_counter == 0)
+    {
+      element.title_ = line;
+    }
+    else if (line_counter == 1)
+    {
+      element.choice_A_ = line;
+    }
+    else if (line_counter == 2)
+    {
+      element.choice_B_ = line;
+    }
+    else 
+      element.text_ = line;
+
+    line_counter++;
+  }
+
+  fclose(file_open);
+  
+  return 0;
+}
+
+char enterCommand()
+{
+  printf("Deine Wahl (A/B)? ");
+  char command;
+  scanf("%c", &command);
+  
+ if(command == 'A')
+    choiceA();
+  else if(command == 'B')
+    choiceB();
+  else
+  {
+    printf("[ERR] Please enter A or B.\n");
+    enterCommand();
+  }
+  return command;
+}
+
+char choiceA()
+{
+  exit(0);
+}
+char choiceB()
+{
   return 0;
 }
